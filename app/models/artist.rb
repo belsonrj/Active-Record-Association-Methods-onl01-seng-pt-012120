455459f5053567096099a27f9101c237472a688e
Rick Belson <belsonrj@aol.com>
2020-04-06 01:00:21 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -18,7 +18,7 @@ class Artist < ActiveRecord::Base
 
   def genre_count
     #return the number of genres associated with the artist
-      genres = self.songs.collect do |song|
+    genres = self.songs.collect do |song|
       song.genre
     end
     genres.compact.count
